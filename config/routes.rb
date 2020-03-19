ce40e7dad4372fb17194002f27c6c9f9912974a5
vagrant <vagrant@localhost.localdomain>
2020-03-19 07:11:28 +0000

    [Finish] 5-6

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resource :post_comments, only: [:create, :destroy]
   end
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
